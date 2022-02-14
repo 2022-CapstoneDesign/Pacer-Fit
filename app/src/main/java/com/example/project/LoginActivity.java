717459ce1aa8f3252514f49bbf16f7f6618013c6
UhDgayeong <gygy3686@naver.com>
2022-02-15 00:49:26 +0900

    LoginActivity, JoinActivity 생성

---
+++ app/src/main/java/com/example/project/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextInputEditText idTxt;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        idTxt = findViewById(R.id.loginIdTxt);
+        // 입력 시 처음 뜨는 키보드 언어 강제 설정(이라는데 효과가 없는 것 같습니다)
+        idTxt.setPrivateImeOptions("defaultInputmode=english;");
+        // 아이디는 한글, 특수문자, 이모지 등은 제한하고 영어 소문자, 숫자만 최대 20글자 입력 허용
+        // (참고 : 비밀번호는 영어(소,대), 숫자, 특수문자만 최대 20글자 입력 가능)
+        idTxt.setFilters(new InputFilter[] {filter, new InputFilter.LengthFilter(20)});
+
+        loginBtn = findViewById(R.id.loginLoginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class)); // 메인 액티비티로 전환
+            }
+        });
+    }
+
+    // 영어(소문자), 숫자만 입력을 위한 필터
+    protected InputFilter filter = (source, start, end, dest, dstart, dend) -> {
+        Pattern ps = Pattern.compile("^[a-z0-9]+$");
+        if (!ps.matcher(source).matches()) {
+            return "";
+        }
+        return null;
+    };
+}
