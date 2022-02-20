4a127b85e1697a0f4878048fb3b27f9aa2021f26
UhDgayeong <gygy3686@naver.com>
2022-02-20 23:47:04 +0900

    커스텀 툴바 적용

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
