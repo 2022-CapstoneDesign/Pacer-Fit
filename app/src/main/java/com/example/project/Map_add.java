988eb2fcbf401bde64725423fac29c36cc24fc15
zini9188 <83208807+zini9188@users.noreply.github.com>
2022-03-07 04:03:19 +0900

    지도 타입 변경 기능 추가 내 위치 추적 및 중지 기능 추가

--- app/src/main/java/com/example/project/Map_add.java
+++ app/src/main/java/com/example/project/Map_add.java
@@ -1,22 +1,43 @@
 package com.example.project;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class Map_add extends AppCompatActivity {
+    int mapOption = 0;
+    MapView mapView = null;
+    private final int ACCESS_FINE_LOCATION = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_add);
-        
+        Button setMapType = findViewById(R.id.map_type);
+        Button startBtn = findViewById(R.id.start_btn);
+        Button stopBtn = findViewById(R.id.stop_btn);
         //지도 표시 (activity_map_add.xml)
-        MapView mapView = new MapView(this); //실제 핸드폰으로 디코딩해야 지도가 나옵니다.
+        mapView = new MapView(this); //실제 핸드폰으로 디코딩해야 지도가 나옵니다.
+
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
 
@@ -35,5 +56,109 @@ public class Map_add extends AppCompatActivity {
         marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
         mapView.addPOIItem(marker);
 
+        // 맵 출력하는 방식을 바꿔주는 버튼입니다
+        setMapType.setOnClickListener(v -> {
+            mapOption += 1;
+            mapOption %= 3;
+            switch (mapOption) {
+                case 0:
+                    mapView.setMapType(MapView.MapType.Standard);
+                    break;
+                case 1:
+                    mapView.setMapType(MapView.MapType.Satellite);
+                    break;
+                case 2:
+                    mapView.setMapType(MapView.MapType.Hybrid);
+                    break;
+
+            }
+        });
+
+        // 시작 버튼을 누르면 GSP 권한 체크 후 추적 시작
+        startBtn.setOnClickListener(v -> {
+            if (checkLocationService()) {
+                permissionCheck();
+            } else {
+                Toast.makeText(this, "GPS를 켜주세요", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 중지 버튼을 누르면 추적 중지
+        stopBtn.setOnClickListener(v -> {
+            stopTracking();
+        });
+
     }
+
+    // GPS 허용 여부 권환 확인
+    private void permissionCheck() {
+        String[] permissions = {
+                Manifest.permission.ACCESS_FINE_LOCATION
+        };
+
+        SharedPreferences preference = getPreferences(MODE_PRIVATE);
+        boolean isFirstCheck = preference.getBoolean("isFirstPermissionCheck", true);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 없는 상태
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // 권한 거절시 다시 한번 물어봄
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("현재 위치를 확인하시려면 위치 권한을 허용해주세요");
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(Map_add.this, permissions, ACCESS_FINE_LOCATION);
+                    }
+                });
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                builder.show();
+            } else {
+                if (isFirstCheck) {
+                    // 최초 권한 요청
+                    preference.edit().putBoolean("isFirstPermissionCheck", false).apply();
+                    ActivityCompat.requestPermissions(Map_add.this, permissions, ACCESS_FINE_LOCATION);
+                } else {
+                    // 다시 묻지 않음 클릭 (앱 정보 화면으로 이동)
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("현재 위치를 확인하시려면 설정에서 위치 권한을 허용해주세요");
+                    builder.setPositiveButton("설정으로 이동", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:$packageName"));
+                            startActivity(intent);
+                        }
+                    });
+                    builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                        }
+                    });
+                    builder.show();
+                }
+            }
+        } else {
+            // 권한이 있는 상태
+            startTracking();
+        }
+    }
+
+    private boolean checkLocationService() {
+        LocationManager locationManger = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManger.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    private void startTracking() {
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+    }
+
+    private void stopTracking() {
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
+
+    }
+
+
 }
