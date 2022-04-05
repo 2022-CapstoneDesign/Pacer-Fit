3f5e4e0a116037eea597fc852ce8bb69a90e148f
UhDgayeong <gygy3686@naver.com>
2022-04-05 17:52:37 +0900

    애니메이션 수정

--- app/src/main/java/com/example/project/Frag1.java
+++ app/src/main/java/com/example/project/Frag1.java
@@ -1,28 +1,14 @@
 package com.example.project;
 
-import static android.content.Context.LOCATION_SERVICE;
-import static android.content.Context.MODE_PRIVATE;
-
 import static com.example.project.TransLocalPoint.TO_GRID;
 
-import android.Manifest;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.LocationManager;
-import android.media.Image;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,38 +16,35 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.project.Map.RecordMapActivity;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
 
 public class Frag1 extends Fragment {
 
@@ -77,6 +60,7 @@ public class Frag1 extends Fragment {
 
 
     private BarChart barChart; // 막대 그래프
+    private LineChart lineChart; // 꺾은선 그래프
 
 
     @Nullable
@@ -140,6 +124,7 @@ public class Frag1 extends Fragment {
                 break;
         }
 
+        // <----------- 막대 그래프 ----------->
         barChart = v.findViewById(R.id.barchart);
         //barChart.setOnChartValueSelectedListener(this);
 
@@ -147,22 +132,39 @@ public class Frag1 extends Fragment {
         // 최근 1달의 운동량 값 받아오기 -> DB 값으로 추후에 수정
         for (int i = 0; i < 30; i++) {
             float rand = (float)Math.round(new Random().nextFloat() * 15000);
-            //rand = rand / 1000;
-            Log.d("RAND", String.valueOf(rand));
+            //Log.d("RAND", String.valueOf(rand));
             barChartValues.add(rand); // 0 ~ 15,000 사이의 랜덤값
         }
 
-        configureChartAppearance();
-        BarData barChartData = createChartData(barChartValues);
+        barchartConfigureAppearance();
+        BarData barChartData = createBarchartData(barChartValues);
         barChart.setData(barChartData); // BarData 전달
         barChart.invalidate(); // BarChart 갱신해 데이터 표시
+
+        // <----------- 꺾은선 그래프 ----------->
+        lineChart = v.findViewById(R.id.linechart);
+
+        ArrayList<Float> lineChartValues = new ArrayList<>();
+        // 최근 1달의 운동량 값 받아오기 -> DB 값으로 추후에 수정
+        for (int i = 0; i < 30; i++) {
+            float rand = (float)Math.round(new Random().nextFloat() * 15000);
+            //Log.d("RAND", String.valueOf(rand));
+            lineChartValues.add(rand); // 0 ~ 15,000 사이의 랜덤값
+        }
+
+        linechartConfigureAppearance();
+        LineData lineChartData = createLinechartData(lineChartValues);
+        lineChart.setData(lineChartData); // LineData 전달
+        lineChart.invalidate(); // LineChart 갱신해 데이터 표시
+
+
 
         return v;
 
     } // onCreateView
 
-    // 막대차트 각종 설정
-    private void configureChartAppearance() {
+    // 막대그래프 각종 설정
+    private void barchartConfigureAppearance() {
         barChart.setTouchEnabled(false); // 터치 유무
         barChart.setPinchZoom(false); // 두 손가락으로 줌인,줌아웃 설정
         barChart.setDrawBarShadow(false); // 그래프의 그림자
@@ -204,8 +206,45 @@ public class Frag1 extends Fragment {
         //axisRight.setEnabled(false); // 오른쪽 y축을 안 보이게 해줌
     }
 
+    // 꺾은선그래프 각종 설정
+    private void linechartConfigureAppearance() {
+        lineChart.setTouchEnabled(false);
+        lineChart.setTouchEnabled(false); // 터치 유무
+        lineChart.setPinchZoom(false); // 두 손가락으로 줌인,줌아웃 설정
+        lineChart.setDrawGridBackground(false); // 격자무늬 유무
+        lineChart.getLegend().setEnabled(false); // legend는 차트의 범례
+        lineChart.getDescription().setEnabled(false); // 우측 하단의 DescriptionLabel 삭제
+        //lineChart.animateY(1500); // 밑에서부터 올라오는 애니메이션 적용
+        lineChart.animateX(1500); // 왼쪽-오른쪽 방향의 애니메이션 적용
+
+        // x축 설정(꺾은선그래프 기준 아래쪽)
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setAxisMaximum(30f);
+        xAxis.setDrawAxisLine(false); // 축 그리기 설정
+        xAxis.setGranularity(5f); // 간격 설정(표시되는 값)
+        xAxis.setTextSize(13f);
+        xAxis.setTextColor(Color.parseColor("#909090"));
+        xAxis.setDrawGridLines(false); // 격자
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // X축 데이터 표시 위치
+        xAxis.setValueFormatter(new MyXAxisValueFormatter());
+
+        YAxis yAxisLeft = lineChart.getAxisLeft();
+        yAxisLeft.setAxisMaximum(15001f); // y축 최대값 설정
+        yAxisLeft.setAxisMinimum(0f); // y축 최소값 설정
+        yAxisLeft.setDrawLabels(false); // 값 표기 설정
+        yAxisLeft.setDrawGridLines(false); // 격자
+        yAxisLeft.setDrawAxisLine(false); // 축 그리기 설정
+
+        // (꺾은선그래프 기준 오른쪽)
+        YAxis axisRight = lineChart.getAxisRight();
+        axisRight.setDrawLabels(false); // 값 표기 설정
+        axisRight.setDrawGridLines(false); // 격자
+        axisRight.setDrawAxisLine(false); // 축 그리기 설정
+
+    }
+
     // 이 함수에서 생성된 BarData를 실제 BarData 객체에 전달하고 BarChart를 갱신해 데이터를 표시
-    private BarData createChartData(ArrayList<Float> chartValues) {
+    private BarData createBarchartData(ArrayList<Float> chartValues) {
         // 1. [BarEntry] BarChart에 표시될 데이터 값 생성
         ArrayList<BarEntry> values = new ArrayList<>();
         for (int i = 0; i < 30; i++) {
@@ -223,6 +262,39 @@ public class Frag1 extends Fragment {
         // 3. [BarData] 보여질 데이터 구성
         BarData data = new BarData(set);
         data.setBarWidth(0.6f);
+        data.setValueTextSize(15);
+
+        return data;
+    }
+
+    // 이 함수에서 생성된 LineData를 실제 LineData 객체에 전달하고 LineChart를 갱신해 데이터를 표시
+    private LineData createLinechartData(ArrayList<Float> chartValues) {
+        // 1. [Entry] LineChart에 표시될 데이터 값 생성
+        ArrayList<Entry> values = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            float x = i;
+            float y = chartValues.get(i);
+            values.add(new Entry(x, y));
+        }
+
+        // 2. [LineDataSet] 단순 데이터를 꺾은선 모양으로 표시, LineChart의 막대 커스텀
+        LineDataSet set = new LineDataSet(values, "꺾은선그래프");
+        set.setDrawIcons(false);
+        set.setDrawValues(false);
+        int green = ContextCompat.getColor(getContext(), R.color.green_project);
+        int gray = ContextCompat.getColor(getContext(), R.color.gray_project);
+        set.setFillColor(green);
+        set.setColor(green);
+        set.setDrawCircleHole(true);
+        set.setCircleColor(green); // 바깥 원 색
+        set.setCircleHoleColor(gray); // 안쪽 원 색
+        set.setLineWidth(1.5f); // 선 두께
+        set.setCircleSize(4f);
+        set.setDrawCircles(true); //선 둥글게 만들기
+        set.setDrawFilled(false); //그래프 밑부분 색칠X
+
+        // 3. [LineData] 보여질 데이터 구성
+        LineData data = new LineData(set);
         data.setValueTextSize(15);
 
         return data;
