001248efeb1a6dc694663f594af85cffdf8b64cc
UhDgayeong <gygy3686@naver.com>
2022-05-28 23:18:08 +0900

    마이페이지 스크롤 문제 수정

---
+++ app/src/main/java/com/example/project/Login/SurveyActivity.java
@@ -0,0 +1,16 @@
+package com.example.project.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project.R;
+
+public class SurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_survey_activity);
+    }
+}
